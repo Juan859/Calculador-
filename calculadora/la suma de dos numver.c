ccefa4d771df9d591b2d4856d9c600dd3913c31a
Juan C. Mate Peña <52606424+Juan859@users.noreply.github.com>
2021-03-25 11:05:09 -0400

    Calculadora
    C/C+++

---
+++ calculadora/la suma de dos numver.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num1;
+   int num2;
+    int suma;
+     printf("introduzca su primer valor:");
+     scanf("%i",&num1);
+      printf("intoduzca su segundo valoe:");
+     scanf("%i",&num2);
+
+    suma= num1 + num2;
+    if(suma=50)
+        {
+      printf("el resultado de la sumatoria  es igual a:%i",suma);
+    }
+
+      else
+        {
+        printf(" introduzca 2 cantidades que de un valor especifico");
+      }
+
+    return 0;
+
+}
